5944323e62a002ad5e071453239f3230633a1e7a
Romano <romanornr@gmail.com>
2018-12-23 02:16:06 +0100

    Improve audit and unit test

--- atomic/audit.go
+++ atomic/audit.go
@@ -4,21 +4,29 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
-	"time"
-
 	"github.com/go-errors/errors"
 	"github.com/viacoin/viad/chaincfg"
 	"github.com/viacoin/viad/txscript"
 	"github.com/viacoin/viad/wire"
 	btcutil "github.com/viacoin/viautil"
+	"time"
 )
 
-type auditContractCmd struct {
+type AuditContractCmd struct {
 	contract   []byte
 	contractTx *wire.MsgTx
 }
 
-func AuditContract(contractHex string, contractTransaction string) {
+type AuditedContract struct {
+	Address                *btcutil.AddressScriptHash `json:"address"`
+	Value                  btcutil.Amount             `json:"value"`
+	RecipientAddress       *btcutil.AddressPubKeyHash `json:"recipient_address"`
+	RecipientRefundAddress *btcutil.AddressPubKeyHash `json:"recipient_refund_address"`
+	LockTime               int64                      `json:"lock_time"`
+	AtomicSwapDataPushes   *txscript.AtomicSwapDataPushes
+}
+
+func AuditContract(contractHex string, contractTransaction string) (*AuditContractCmd) {
 	contract, err := hex.DecodeString(contractHex)
 	if err != nil {
 		fmt.Errorf("failed to decode contract: %v\n", err)
@@ -34,11 +42,10 @@ func AuditContract(contractHex string, contractTransaction string) {
 		fmt.Errorf("failed to decode transaction: %v\n", err)
 	}
 
-	cmd := &auditContractCmd{contract: contract, contractTx: &contractTx}
-	cmd.runCommand()
+	return &AuditContractCmd{contract: contract, contractTx: &contractTx}
 }
 
-func (cmd *auditContractCmd) runCommand() error {
+func (cmd *AuditContractCmd) runAudit() (AuditedContract, error) {
 	contractHash160 := btcutil.Hash160(cmd.contract)
 	contractOut := -1
 	for i, out := range cmd.contractTx.TxOut {
@@ -52,41 +59,49 @@ func (cmd *auditContractCmd) runCommand() error {
 	}
 
 	if contractOut == -1 {
-		return errors.New("transaction does not contain the contract output")
+		return AuditedContract{}, errors.New("transaction does not contain the contract output")
 	}
 
 	pushes, err := txscript.ExtractAtomicSwapDataPushes(0, cmd.contract)
 	if err != nil {
-		return err
+		return AuditedContract{}, err
 	}
 	if pushes == nil {
-		return errors.New("contract is not an atomic swap script recognized by this tool")
+		return AuditedContract{}, errors.New("contract is not an atomic swap script recognized by this tool")
 	}
 	if pushes.SecretSize != secretSize {
-		return fmt.Errorf("contract specifies strange range secret size: %v\n", pushes.SecretSize)
+		return AuditedContract{}, fmt.Errorf("contract specifies strange range secret size: %v\n", pushes.SecretSize)
 	}
 
 	contractAddr, err := btcutil.NewAddressScriptHash(cmd.contract, &chaincfg.MainNetParams)
 	if err != nil {
-		return err
+		return AuditedContract{}, err
 	}
 
 	recipientAddr, err := btcutil.NewAddressPubKeyHash(pushes.RecipientHash160[:], &chaincfg.MainNetParams)
 	if err != nil {
-		return err
+		return AuditedContract{}, err
 	}
 	refundAddr, err := btcutil.NewAddressPubKeyHash(pushes.RefundHash160[:], &chaincfg.MainNetParams)
 	if err != nil {
-		return err
+		return AuditedContract{}, err
 	}
 
-	fmt.Printf("Contract address:        %v\n", contractAddr)
-	fmt.Printf("Contract value:          %v\n", btcutil.Amount(cmd.contractTx.TxOut[contractOut].Value))
-	fmt.Printf("Recipient address:       %v\n", recipientAddr)
-	fmt.Printf("Recipient refund address: %v\n\n", refundAddr)
+	contractValue := btcutil.Amount(cmd.contractTx.TxOut[contractOut].Value)
 
-	if pushes.LockTime >= int64(txscript.LockTimeThreshold) {
-		t := time.Unix(pushes.LockTime, 0)
+	contract := AuditedContract{Address: contractAddr, Value: contractValue, RecipientAddress: recipientAddr, RecipientRefundAddress: refundAddr, LockTime: pushes.LockTime, AtomicSwapDataPushes:pushes}
+
+	return contract, nil
+}
+
+func (contract AuditedContract) show() error{
+	fmt.Printf("Contract address:        %v\n", contract.Address)
+	fmt.Printf("Contract value:          %v\n", contract.Value)
+	fmt.Printf("Recipient address:       %v\n", contract.RecipientAddress)
+	fmt.Printf("Recipient refund address: %v\n\n", contract.RecipientRefundAddress)
+
+	if contract.AtomicSwapDataPushes.LockTime >= int64(txscript.LockTimeThreshold) {
+		t := time.Unix(contract.AtomicSwapDataPushes.LockTime, 0)
 		fmt.Printf("Locktime: %v\n", t.UTC())
 		reachedAt := time.Until(t).Truncate(time.Second)
 		if reachedAt > 0 {
@@ -95,7 +110,7 @@ func (cmd *auditContractCmd) runCommand() error {
 			fmt.Printf("Contract refund time lock has expired !\n")
 			return nil
 		}
-		fmt.Printf("Locktime: block %v\n", pushes.LockTime)
+		fmt.Printf("Locktime: block %v\n", contract.AtomicSwapDataPushes.LockTime)
 	}
-	return err
+	return nil
 }
--- atomic/audit_test.go
+++ atomic/audit_test.go
@@ -1,7 +1,38 @@
 package atomic
 
-import "testing"
+import (
+	btcutil "github.com/viacoin/viautil"
+	"testing"
+)
 
 func TestAuditContract(t *testing.T) {
-	AuditContract("6382012088a82095b1269e67e58860bb591ccec2efd34700b8fbe56b808e0e1ca66c77c3785d888876a91424cc424c1e5e977175d2b20012554d39024bd68f6704d4441e5cb17576a91424cc424c1e5e977175d2b20012554d39024bd68f6888ac", "0200000001d6af699222767cb7c62ce56bd50a070acb44024095cbfdbad780cb8c10b62023010000006b48304502210096ae3d8bc5b863e99316e6a9a94cd4194c0b2502953725262fa7c8b764e1cf16022071819d34853db65a03aaab9143f98e35f9081db92656a1dd6984f4ce5fb22683012102a7b08bb2a3609223a185761231d815e287ec13b74ccff3feb274253f7737356affffffff01a08601000000000017a914db8bf8b9c38a896e814b43e5f2e0b8f580f8d1ec8700000000")
+	contract, err := AuditContract("6382012088a82095b1269e67e58860bb591ccec2efd34700b8fbe56b808e0e1ca66c77c3785d888876a91424cc424c1e5e977175d2b20012554d39024bd68f6704d4441e5cb17576a91424cc424c1e5e977175d2b20012554d39024bd68f6888ac", "0200000001d6af699222767cb7c62ce56bd50a070acb44024095cbfdbad780cb8c10b62023010000006b48304502210096ae3d8bc5b863e99316e6a9a94cd4194c0b2502953725262fa7c8b764e1cf16022071819d34853db65a03aaab9143f98e35f9081db92656a1dd6984f4ce5fb22683012102a7b08bb2a3609223a185761231d815e287ec13b74ccff3feb274253f7737356affffffff01a08601000000000017a914db8bf8b9c38a896e814b43e5f2e0b8f580f8d1ec8700000000").runAudit()
+	if err != nil {
+		t.Errorf("error decoding the contract hex and contract transaction hex")
+	}
+
+	expectedAddress := "EdAm9zSqK24KgHDZ9WmyQ9A98Ytwff5g8d"
+	if contract.Address.String() != expectedAddress {
+		t.Errorf("Expected contract address to be %s but instead got %s\n", expectedAddress, contract.Address.String())
+	}
+
+	expectedValue, _  := btcutil.NewAmount(0.001)
+	if contract.Value != expectedValue {
+		t.Errorf("Expected contract value to be %v but got %v instead\n", expectedValue, contract.Value)
+	}
+
+	expectedLockTime := int64(1545487572)
+	if contract.LockTime != expectedLockTime {
+		t.Errorf("Expected contract locktime to be %d but got %d instead\n", expectedLockTime, contract.LockTime)
+	}
+
+	expectedRecipientAddress := "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A"
+	if expectedRecipientAddress != contract.RecipientRefundAddress.String() {
+		t.Errorf("Expected contract recipient address to be %s but got %s instead\n", expectedRecipientAddress, contract.RecipientRefundAddress.String())
+	}
+
+	expectedRecipientRefundAddress := "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A"
+	if contract.RecipientRefundAddress.String() != expectedRecipientRefundAddress {
+		t.Errorf("Expected contract recipient refund address to be %s but got %s instead\n", expectedRecipientRefundAddress, contract.RecipientRefundAddress.String())
+	}
 }
