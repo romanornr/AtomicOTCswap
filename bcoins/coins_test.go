9d647a1bc6f0b67165f270716f01e7b60ada6e68
Romano <romanornr@gmail.com>
2018-12-27 20:41:23 +0100

    amount unit test

--- bcoins/coins_test.go
+++ bcoins/coins_test.go
@@ -1,24 +1,18 @@
 package bcoins
 
 import (
-	//"fmt"
-	//btcutil "github.com/viacoin/viautil"
-	"fmt"
 	"testing"
 )
 
 var viacoin = Coin{}
+
 func TestBtcutil_GetBtcUtil(t *testing.T) {
 	viacoin.Symbol = "via"
 
-
 	input, _ := NewAmount(0.001)
+	expected := "0.001 VIA"
 
-	fmt.Println(input)
-
-	//expected, _ := btcutil.NewAmount(0.001)
-
-	//if input.String() != expected.String() {
-	//	t.Errorf("error expected %v but got %v instead\n", expected, input)
-	//}
+	if input.String() != expected {
+		t.Errorf("error expected %v but got %v instead\n", expected, input)
+	}
 }
