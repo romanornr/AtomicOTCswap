f6fae4b6e6a0c264d6d74829ef1ecd1bcbb7ca09
Romano <romanornr@gmail.com>
2019-01-15 19:54:10 +0100

    [router] Add comment api audit

--- server/router.go
+++ server/router.go
@@ -17,10 +17,14 @@ func createRouter() *mux.Router {
 	return r
 }
 
+// audit a contract by giving the coin symbol, contract hex and contract transaction
+// from the contract which needs to be audited
 func AuditHandler(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	coin, contractHex, contractTransaction := params["coin"], params["contractHex"], params["contractTransaction"]
-	contract, _ := atomic.AuditContract(coin, contractHex, contractTransaction)
-	fmt.Println(contract)
-		json.NewEncoder(w).Encode(&contract)
+	contract, err := atomic.AuditContract(coin, contractHex, contractTransaction)
+	if err != nil {
+		fmt.Sprintf("%s\n", err)
+	}
+	json.NewEncoder(w).Encode(&contract)
 }
