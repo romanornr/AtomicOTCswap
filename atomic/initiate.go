13e06437f1bdac903a96aa535b68911e6191de84
Romano <romanornr@gmail.com>
2019-01-16 01:00:15 +0100

    initiate contract by using POST request

--- atomic/initiate.go
+++ atomic/initiate.go
@@ -11,52 +11,68 @@ import (
 	"strings"
 	"time"
 )
+
 type initiateCmd struct {
 	counterParty2Addr *btcutil.AddressPubKeyHash
 	amount            btcutil.Amount
 }
 
+type InitiatedContract struct {
+	Coin string
+	Ticker string
+	ContractAmount float64
+	ContractFee float64
+	ContractRefundFee float64
+	ContractAddress string
+	ContractHex string
+	ContractTransactionID string
+	ContractTransactionHex string
+	RefundTransactionID string
+	RefundTransaction string
+	Secret string
+	SecretHash string
+}
 
-func Initiate(coinTicker string, participantAddr string, wif *btcutil.WIF, amount float64) error {
+func Initiate(coinTicker string, participantAddr string, amount float64, wif *btcutil.WIF) (InitiatedContract, error) {
 
 	coin, err := bcoins.SelectCoin(coinTicker)
 	if err != nil {
-		return err
+		return InitiatedContract{}, err
 	}
 
 	chaincfg.Register(coin.Network.ChainCgfMainNetParams())
 
 	counterParty2Addr, err := btcutil.DecodeAddress(participantAddr, coin.Network.ChainCgfMainNetParams())
 	if err != nil {
-		return fmt.Errorf("failed to decode the address from the participant: %s\n", err)
+		return InitiatedContract{}, fmt.Errorf("failed to decode the address from the participant: %s\n", err)
 	}
 
 	counterParty2AddrP2KH, ok := counterParty2Addr.(*btcutil.AddressPubKeyHash)
 	if !ok {
-		return errors.New("participant address is not P2KH")
+		return InitiatedContract{}, errors.New("participant address is not P2KH")
 	}
 
 	amount2, err := btcutil.NewAmount(amount)
 	if err != nil {
-		return err
+		return InitiatedContract{}, err
 	}
 
 	cmd := &initiateCmd{counterParty2Addr: counterParty2AddrP2KH, amount: amount2}
 	return cmd.runCommand(wif, &coin, amount)
 }
 
-func (cmd *initiateCmd) runCommand(wif *btcutil.WIF, coin *bcoins.Coin, amount float64) error {
+func (cmd *initiateCmd) runCommand(wif *btcutil.WIF, coin *bcoins.Coin, amount float64) (InitiatedContract, error) {
 	var secret [secretSize]byte
 	_, err := rand.Read(secret[:])
 	if err != nil {
-		return err
+		return InitiatedContract{}, err
 	}
 
 	secretHash := sha256Hash(secret[:])
 	locktime := time.Now().Add(12 * time.Hour).Unix() // NEED TO CHANGE
 
 	build, err := buildContract(&contractArgs{
-		coin1: coin,
+		coin1:      coin,
 		them:       cmd.counterParty2Addr,
 		amount:     cmd.amount,
 		locktime:   locktime,
@@ -64,30 +80,40 @@ func (cmd *initiateCmd) runCommand(wif *btcutil.WIF, coin *bcoins.Coin, amount f
 	}, wif)
 
 	if err != nil {
-		return err
+		return InitiatedContract{}, err
 	}
 
 	ticker := strings.ToUpper(coin.Symbol)
 	refundTxHash := build.refundTx.TxHash()
 
-	fmt.Printf("Secret:      %x\n", secret)
-	fmt.Printf("Secret hash: %x\n\n", secretHash)
-
-	fmt.Printf("Contract amount: %v %s\n", amount, ticker)
-	fmt.Printf("Contract fee: %v %s\n", build.contractFee.ToBTC(), ticker)
-	fmt.Printf("Refund fee:   %v %s\n", build.refundFee.ToBTC(), ticker)
-	fmt.Printf("Contract (%v):\n", build.contractP2SH)
-	fmt.Printf("%x\n\n", build.contract)
 	var contractBuf bytes.Buffer
 	contractBuf.Grow(build.contractTx.SerializeSize())
 	build.contractTx.Serialize(&contractBuf)
-	fmt.Printf("Contract transaction (%v):\n", build.contractTxHash)
-	fmt.Printf("%x\n\n", contractBuf.Bytes())
+
 	var refundBuf bytes.Buffer
 	refundBuf.Grow(build.refundTx.SerializeSize())
 	build.refundTx.Serialize(&refundBuf)
-	fmt.Printf("Refund transaction (%v):\n", &refundTxHash)
-	fmt.Printf("%x\n\n", refundBuf.Bytes())
 
-	return nil
+	contract := InitiatedContract{
+
+		Secret: fmt.Sprintf("%x", secret),
+		SecretHash: fmt.Sprintf("%x", secretHash),
+
+		Coin: coin.Name,
+		Ticker: ticker,
+
+		ContractAmount:amount,
+		ContractFee: build.contractFee.ToBTC(),
+		ContractRefundFee: build.refundFee.ToBTC(),
+
+		ContractAddress: fmt.Sprintf("%v", build.contractP2SH),
+		ContractHex: fmt.Sprintf("%x", build.contract),
+
+		ContractTransactionID: fmt.Sprintf("%x", build.contractTxHash),
+		ContractTransactionHex: fmt.Sprintf("%x", contractBuf.Bytes()),
+
+		RefundTransactionID: fmt.Sprintf("%v", &refundTxHash),
+		RefundTransaction: fmt.Sprintf("%x", refundBuf.Bytes()),
+	}
+	return contract, nil
 }
--- server/router.go
+++ server/router.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/romanornr/AtomicOTCswap/atomic"
+	btcutil "github.com/viacoin/viautil"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 
@@ -13,8 +16,31 @@ func createRouter() *mux.Router {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api").Subrouter()
 	api.HandleFunc("/audit/{coin}/{contractHex}/{contractTransaction}", AuditHandler).Methods("GET")
+	api.HandleFunc("/initiate", InitiateHandler).Methods("POST")
 	http.Handle("/", r)
 	return r
+}
+
+// initiate a contract by parsing the post request
+// it parses the coin symbol, counter party address, amount and the wif
+func InitiateHandler(w http.ResponseWriter, req *http.Request) {
+
+	amount, err := strconv.ParseFloat(req.FormValue("amount"), 64)
+	if err != nil {
+		log.Printf("amount should be a float. example: 0.02")
+	}
+
+	wif, err := btcutil.DecodeWIF(req.FormValue("wif"))
+	if err != nil {
+		log.Printf("error decoding private key in wif format: %s\n", err)
+	}
+
+	contract, err := atomic.Initiate(req.FormValue("coin"), req.FormValue("counter_party_2_addr"), amount, wif)
+	if err != nil {
+		log.Printf("erorr initiating contract: %s\n", err)
+	}
+
+	json.NewEncoder(w).Encode(contract)
 }
 
 // audit a contract by giving the coin symbol, contract hex and contract transaction
