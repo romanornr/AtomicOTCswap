a3a782c7d4bb9f3f6d62a14473e1e906a47d9c78
Romano <romanornr@gmail.com>
2019-02-28 17:36:19 +0100

    Add license

--- bcoins/transaction.go
+++ bcoins/transaction.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 Romano (Viacoin developer)
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package bcoins
 
 type Transaction struct {
--- insight/utxo.go
+++ insight/utxo.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 Romano (Viacoin developer)
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package insight
 
 import (
