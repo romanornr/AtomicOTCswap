ce599251c0c3be1dda6fab2cd9d19d10f0124d4c
Romano <romanornr@gmail.com>
2019-02-28 17:32:34 +0100

    Add ISC license

--- insightjson/transaction.go
+++ insightjson/transaction.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 The Decred developers
+// Copyright (c) 2019 Romano (Viacoin developer)
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package insightjson
 
 type Tx struct {
